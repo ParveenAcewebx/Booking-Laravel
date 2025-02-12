768a2b0700276a4b77d3be192f194431254f1a3d
pooja-webxpert <pooja.webxpert@gmail.com>
2025-02-12 10:34:32 +0530

    added the breadcrumb in edit and add form

--- resources/views/form/form.blade.php
+++ resources/views/form/form.blade.php
@@ -3,6 +3,43 @@
 @section('content')
 <div class="pcoded-main-container">
     <div class="pcoded-content">
+    <div class="page-header">
+			<div class="page-block">
+				<div class="row align-items-center">
+					<div class="col-md-12">
+						<div class="page-header-title">
+							<h5>Form Add</h5>
+						</div>
+						<ul class="breadcrumb">
+							<li class="breadcrumb-item"><a href="{{route('home') }}"><i class="feather icon-home"></i></a></li>
+							<li class="breadcrumb-item"><a href="#!">Form</a></li>
+							<li class="breadcrumb-item"><a href="#!">Add Form</a></li>
+						</ul>
+
+                        @if(session('success'))
+                        <div id="exampleModalCenter" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+							<div class="modal-dialog modal-dialog-centered" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="exampleModalCenterTitle">Message</h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+									</div>
+									<div class="modal-body">
+										<p class="mb-0">{{ session('success') }}</p>
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn  btn-secondary" data-dismiss="modal">Okay</button>
+										
+									</div>
+								</div>
+							</div>
+						</div>
+						<button  style="display:none;" id="mymodelsformessage" type="button" class="btn  btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Launch demo modal</button>
+                        @endif
+					</div>
+				</div>
+			</div>
+		</div>
                     <div class="card">
                         <div class="card-header">
                         <h5>Add Form </h5>
