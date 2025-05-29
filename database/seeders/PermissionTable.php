6c5cec4775c0bc7c1fb15162d4864709dbcd5541
webbninja2 <45854084+webbninja2@users.noreply.github.com>
2025-05-29 12:50:46 +0530

    Update seeders records (#47)
    Co-authored-by: parveen <parveen@acewebx.com>

--- database/seeders/PermissionTable.php
+++ database/seeders/PermissionTable.php
@@ -10,8 +10,16 @@ class PermissionTable extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $permissions = ['view', 'edit', 'manage'];
+
+        foreach ($permissions as $index => $permission) {
+            Permission::create([
+                'id' => $index + 1,
+                'name' => $permission,
+                'guard_name' => 'web',
+            ]);
+        }
     }
 }
--- database/seeders/RoleHasPermissionsTable.php
+++ database/seeders/RoleHasPermissionsTable.php
@@ -10,8 +10,18 @@ class RoleHasPermissionsTable extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $admin = Role::findByName('Administrator');
+        $admin->givePermissionTo(Permission::all());
+
+        $staff = Role::findByName('Staff');
+        $staff->givePermissionTo(['view', 'edit']);
+
+        $manager = Role::findByName('Booking Manager');
+        $manager->givePermissionTo(['view', 'manage']);
+
+        $customer = Role::findByName('Customer');
+        $customer->givePermissionTo(['view']);
     }
 }
