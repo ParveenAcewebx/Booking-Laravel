c94b8d65a7eeafe284b1b80c557bd1e93ad53a55
Pooja Webxpert <pooja-webxpert@gmail.com>
2025-06-02 11:46:36 +0530

    Bydefault set customer when any user register form on frontend

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,9 +12,9 @@ use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
-use DB; 
-use Carbon\Carbon; 
-use Mail; 
+use DB;
+use Carbon\Carbon;
+use Mail;
 
 use Illuminate\Support\Str;
 
@@ -22,16 +22,17 @@ use Illuminate\Support\Str;
 class UserController extends Controller
 {
 
-    public function index(){
-        $alluser=User::all();
+    public function index()
+    {
+        $alluser = User::all();
         $currentUserId = Auth::id();
         return view('user.userlist', ['alluser' => $alluser]);
-
     }
 
-    public function userAdd(){
-        $allRole=Role::all();
-        return view('user.usercreate',['allRoles' => $allRole]);
+    public function userAdd()
+    {
+        $allRole = Role::all();
+        return view('user.usercreate', ['allRoles' => $allRole]);
     }
 
     public function userSave(Request $request)
@@ -43,24 +44,24 @@ class UserController extends Controller
             'password' => 'required|min:6|confirmed',
             'avatar' => 'required|nullable|image|mimes:jpeg,png,jpg,gif|max:2048'
         ]);
-    
+
         // Initialize avatar path
         $avatarPath = null;
-    
+
         // Check if an avatar file is uploaded
         if ($request->hasFile('avatar')) {
             // Store the uploaded image in the 'avatars' folder
             $avatarPath = $request->file('avatar')->store('avatars', 'public');
         }
-    
+
         // Save user data to the database
         $user = User::create([
             'name' => $request->username,
             'email' => $request->email,
-            'password' => bcrypt($request->password), 
+            'password' => bcrypt($request->password),
             'avatar' => $avatarPath, // Save the uploaded avatar path or null
         ]);
-        $userRole=Role::find($request->role);
+        $userRole = Role::find($request->role);
         $user->assignRole($userRole);
         // Check if the user was created successfully
         if ($user) {
@@ -70,22 +71,22 @@ class UserController extends Controller
         }
     }
 
-    public function userEdit($id=null)
+    public function userEdit($id = null)
     {
-        if($id==null){
-            $id= Auth::id();
+        if ($id == null) {
+            $id = Auth::id();
         }
         $user = User::findOrFail($id);
-       
+
         $user->unsetRelation('roles')->unsetRelation('permissions');
         $roles = $user->roles;
-        return view('user.useredit', ['user' => $user,'allRoles'=> Role::all(),'currentRole'=> $roles[0]->id]);
+        return view('user.useredit', ['user' => $user, 'allRoles' => Role::all(), 'currentRole' => $roles[0]->id]);
     }
-    
-    public function userUpdate(Request $request, $id=null)
+
+    public function userUpdate(Request $request, $id = null)
     {
-        if($id==null){
-            $id= Auth::id();
+        if ($id == null) {
+            $id = Auth::id();
         }
         // Validate input data
         $request->validate([
@@ -94,53 +95,53 @@ class UserController extends Controller
             'password' => 'nullable|min:6|confirmed',
             'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048'
         ]);
-    
+
         $user = User::findOrFail($id);
-    
+
         // Handle avatar update
         $avatarPath = $user->avatar; // Keep existing avatar
         if ($request->hasFile('avatar')) {
             $avatarPath = $request->file('avatar')->store('avatars', 'public');
         }
-    
+
         // Update user details
         $user->update([
             'name' => $request->username,
             'email' => $request->email,
             'avatar' => $avatarPath,
         ]);
-    
+
         // Update password if provided
         if ($request->password) {
             $user->update(['password' => bcrypt($request->password)]);
         }
-        $userRole=Role::find($request->role);
+        $userRole = Role::find($request->role);
         $user->roles()->detach();
         $user->assignRole($userRole);
         return back()->with('success', 'User updated successfully!');
     }
-    
-    
-    public function userDelete($id) {
+
+
+    public function userDelete($id)
+    {
 
         $user = User::find($id);
-        $authuser_id =Auth::user()->id;
-        $username=$user->name;
-        if($authuser_id!=$id){
-           $user->delete();
-           return response()->json(['success' => true]);
-        }else{
+        $authuser_id = Auth::user()->id;
+        $username = $user->name;
+        if ($authuser_id != $id) {
+            $user->delete();
+            return response()->json(['success' => true]);
+        } else {
             return response()->json(['success' => 'login', 'message' => 'Item not found']);
-
         }
     }
 
     public function createUser()
     {
-        $user=User::find(1);
+        $user = User::find(1);
         $user->assignRole('writer');
         return response()->json(['message' => 'User created successfully']);
-    } 
+    }
 
     public function showLoginForm()
     {
@@ -151,12 +152,12 @@ class UserController extends Controller
     {
         $credentials = $request->only('email', 'password');
         $remember = $request->has('rememberme');
-        if (Auth::attempt($credentials,$remember)) {
-           if(Auth::user()->hasRole('Customer')){
-            return redirect()->intended('/welcome');
-           }else{
-            return redirect()->intended('/');
-           }
+        if (Auth::attempt($credentials, $remember)) {
+            if (Auth::user()->hasRole('Customer')) {
+                return redirect()->intended('/welcome');
+            } else {
+                return redirect()->intended('/');
+            }
         }
         return redirect('/login')->with('error', 'Invalid credentials. Please try again.');
     }
@@ -173,28 +174,37 @@ class UserController extends Controller
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
-        User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        $role_id = 4;
+        $userRole = Role::find($role_id);
+        $user->assignRole($userRole);
+
+
         return redirect('/login')->with('success', 'Registration successful! Please log in.');
     }
 
-    public function permissionCheck(){
+    public function permissionCheck()
+    {
         echo "permission has given";
     }
 
-    public function logout(){
+    public function logout()
+    {
         Auth::logout();
         return redirect('/login');
     }
 
-    public function todo(){
+    public function todo()
+    {
         return redirect('/home');
     }
-    
-    public function forgotPassword(){
+
+    public function forgotPassword()
+    {
         return view('auth.forgot-password');
     }
 
@@ -205,21 +215,21 @@ class UserController extends Controller
         ]);
         $token = Str::random(64);
         DB::table('password_reset_tokens')->updateOrInsert(
-            ['email' => $request->email], 
+            ['email' => $request->email],
             [
-                'token' => $token,          
-                'created_at' => Carbon::now() 
+                'token' => $token,
+                'created_at' => Carbon::now()
             ]
         );
-        
+
         $resetLink = route('password.reset', ['token' => $token]);
 
-        Mail::send([], [], function($message) use($request, $resetLink) {
+        Mail::send([], [], function ($message) use ($request, $resetLink) {
             $message->to($request->email)
-                    ->subject('Reset Password')
-                    ->setBody('Here is your reset password link: <a href="' . $resetLink . '">Click here to reset your password</a>', 'text/html');
+                ->subject('Reset Password')
+                ->setBody('Here is your reset password link: <a href="' . $resetLink . '">Click here to reset your password</a>', 'text/html');
         });
-             
+
         return back()->with('message', 'We have e-mailed your password reset link!');
     }
 
@@ -236,24 +246,25 @@ class UserController extends Controller
             'password_confirmation' => 'required'
         ]);
         $updatePassword = DB::table('password_reset_tokens')
-                            ->where([
-                              'email' => $request->email, 
-                              'token' => $request->token
-                            ])
-                            ->first();
-        if(!$updatePassword){
+            ->where([
+                'email' => $request->email,
+                'token' => $request->token
+            ])
+            ->first();
+        if (!$updatePassword) {
             return back()->withInput()->with('error', 'Invalid token!');
         }
         $user = User::where('email', $request->email)->update(['password' => Hash::make($request->password)]);
-        DB::table('password_reset_tokens')->where(['email'=> $request->email])->delete();
+        DB::table('password_reset_tokens')->where(['email' => $request->email])->delete();
         return redirect('/login')->with('message', 'Your password has been changed!');
     }
     public function welcome()
     {
         return view('auth.welcome');
     }
-   // Create roles
-   public function userrole(){
+    // Create roles
+    public function userrole()
+    {
         $adminRole = Role::firstOrCreate(['name' => 'Administrator']);
         $staffRole = Role::firstOrCreate(['name' => 'Staff']);
         $bookingRole = Role::firstOrCreate(['name' => 'Booking Manager']);
@@ -279,5 +290,5 @@ class UserController extends Controller
         foreach ($users as $userToUpdate) {
             $userToUpdate->assignRole($bookingRole);
         }
- }
+    }
 }
