260dd78a3a40100bbc284458a8d47ca9646659c8
Pooja Webxpert <pooja-webxpert@gmail.com>
2025-06-06 14:46:26 +0530

    Changes User Status default 1

--- database/migrations/2025_06_06_065213_add_status_to_users_table.php
+++ database/migrations/2025_06_06_065213_add_status_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('status')->default(0)->after('email_verified_at');
+            $table->integer('status')->default(1)->after('email_verified_at');
         });
     }
 
