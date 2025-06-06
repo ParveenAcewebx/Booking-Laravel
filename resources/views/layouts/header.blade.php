7b4818ae5d8577effde65658c39c6b74d7ee7d88
webbninja2 <45854084+webbninja2@users.noreply.github.com>
2025-06-06 11:27:32 +0530

    Add logo acebooking (#74)
    Co-authored-by: parveen <parveen@acewebx.com>

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,9 +4,7 @@
 	<div class="m-header">
 		<a class="mobile-menu" id="mobile-collapse" href="#!"><span></span></a>
 		<a href="#!" class="b-brand">
-			<!-- ========   change your logo hear   ============ -->
-			<!-- ========   change your logo hear   ============ -->
-			<img src="assets/images/logo.png" alt="" class="logo">	
+			<img src="{{ asset('assets/images/logo.png') }}" alt="ace-logo" class="logo">	
 		</a>
 		<a href="#!" class="mob-toggler">
 			<i class="feather icon-more-vertical"></i>
