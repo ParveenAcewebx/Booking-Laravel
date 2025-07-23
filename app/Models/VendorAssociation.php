f5d6a15426a42e9f7dc2ee8b55a6da04f2c74063
Pooja Webxpert <pooja-webxpert@gmail.com>
2025-07-23 18:27:02 +0530

    Add Service Structure In staff Menu

--- app/Models/VendorAssociation.php
+++ app/Models/VendorAssociation.php
@@ -18,6 +18,6 @@ class VendorAssociation extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
