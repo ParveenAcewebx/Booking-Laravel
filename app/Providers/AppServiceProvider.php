350e4dc3fbe4a2a59d093c65cee34780c474eefe
Pooja Webxpert <pooja-webxpert@gmail.com>
2025-07-24 17:00:54 +0530

    Update Form Code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
             return 'Hello, World!';
         });
 
-        Shortcode::register('user-information', function ($shortcodeAttrs) {
-
+        Shortcode::register('user-information', function ($shortcodeAttrs, $class) {
+            $c = $class;
             $firstName = $shortcodeAttrs['first_name'] ?? '';
             $lastName  = $shortcodeAttrs['last_name'] ?? '';
             $email     = $shortcodeAttrs['email'] ?? '';
@@ -33,24 +33,24 @@ class AppServiceProvider extends ServiceProvider
 
             $userForm  = '';
 
-            $userForm .= "<div class='form-group'>";
-            $userForm .= "<label for='first_name'>First Name</label>";
-            $userForm .= "<input type='text' name='dynamic[first_name]' id='first_name' class='form-control' value='" . e($firstName) . "' placeholder='First Name' required>";
+            $userForm .= "<div class='form-group {$c['group']}'>";
+            $userForm .= "<label for='first_name'class='{$c['label']}'>First Name</label>";
+            $userForm .= "<input type='text' name='dynamic[first_name]' id='first_name' class='form-control {$c['input']}' value='" . e($firstName) . "' placeholder='First Name' required>";
             $userForm .= "</div>";
 
-            $userForm .= "<div class='form-group'>";
-            $userForm .= "<label for='last_name'>Last Name</label>";
-            $userForm .= "<input type='text' name='dynamic[last_name]' id='last_name' class='form-control' value='" . e($lastName) . "' placeholder='Last Name' required>";
+            $userForm .= "<div class='form-group {$c['group']}'>";
+            $userForm .= "<label for='last_name' class='{$c['label']}'>Last Name</label>";
+            $userForm .= "<input type='text' name='dynamic[last_name]' id='last_name' class='form-control {$c['input']}' value='" . e($lastName) . "' placeholder='Last Name' required>";
             $userForm .= "</div>";
 
-            $userForm .= "<div class='form-group'>";
-            $userForm .= "<label for='email'>Email</label>";
-            $userForm .= "<input type='email' name='dynamic[email]' id='email' class='form-control' value='" . e($email) . "' placeholder='Email Address' required>";
+            $userForm .= "<div class='form-group {$c['group']}' >";
+            $userForm .= "<label for='email'class='{$c['label']}'>Email</label>";
+            $userForm .= "<input type='email' name='dynamic[email]' id='email' class='form-control {$c['input']}' value='" . e($email) . "' placeholder='Email Address' required>";
             $userForm .= "</div>";
 
-            $userForm .= "<div class='form-group'>";
-            $userForm .= "<label for='phone'>Phone</label>";
-            $userForm .= "<input type='tel' name='dynamic[phone]' id='phone' class='form-control' value='" . e($phone) . "' placeholder='Phone Number' maxlength='10' oninput=\"this.value = this.value.replace(/[^0-9]/g, '')\">";
+            $userForm .= "<div class='form-group {$c['group']}'>";
+            $userForm .= "<label for='phone' class='{$c['label']}'>Phone</label>";
+            $userForm .= "<input type='tel' name='dynamic[phone]' id='phone' class='form-control {$c['input']}' value='" . e($phone) . "' placeholder='Phone Number' maxlength='10' oninput=\"this.value = this.value.replace(/[^0-9]/g, '')\">";
             $userForm .= "</div>";
 
             return $userForm;
