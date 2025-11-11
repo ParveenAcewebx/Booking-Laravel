cc75677919e4ae9d438c32fe3cfff99608ee2acc
pooja <pooja.webxpert@gmail.com>
2025-11-11 15:03:52 +0530

    add button for continue with gmail

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,7 +43,7 @@
                         </form>
                         <hr>
                          <button type="submit" class="btn btn-block btn-danger mb-4">Login With Google</button>
-                         <button type="submit" class="btn btn-block btn-primary mb-4">Login With Facebook</button>
+                         <button type="submit" class="btn btn-block btn-primary mb-4">Login With Gmail</button>
                          <p class="mb-2 text-muted">Forgot password? <a href="{{route('password.request')}}" class="f-w-400">Reset</a></p>
                         <p class="mb-2 text-muted">Don't have an account? <a href="{{route('register')}}" class="f-w-400">Signup</a></p>
                     </div>
