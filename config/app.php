45fd235ddce411ddbbf4f2f15b87f3180536f0e5
pooja <pooja.webxpert@gmail.com>
2025-11-11 17:39:30 +0530

    add functionlty for login with google

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\SmtpConfigServiceProvider::class,
         App\Providers\PageServiceProvider::class,
+        
     ])->toArray(),
 
     /*
