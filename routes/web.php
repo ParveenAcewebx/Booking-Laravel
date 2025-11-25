7407173538206cb4499a376c3b8cc026eb9de1e7
pooja-webxpert <50840378+pooja-webxpert@users.noreply.github.com>
2025-11-25 17:47:41 +0530

    fix login with google issue (#223)

--- routes/web.php
+++ routes/web.php
@@ -80,7 +80,6 @@ Route::middleware('guest')->group(function () {
     Route::post('password/reset', [UserController::class, 'reset'])->name('password.update');
 });
 
-Route::post('/settings/google-login', [SettingsController::class, 'updateGoogleLogin'])->name('settings.google.login');
 
 // Mail approve link to users.
 Route::get('/email/verify', function () {
